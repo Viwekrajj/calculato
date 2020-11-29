e6c8b4db6cd0d5203c228833f7ec32220834eb73
vivek ranjan <41664287+Viwekrajj@users.noreply.github.com>
2020-11-29 14:39:15 +0530

    Update vcDenofProjects.ts

--- src/viewConnectors/vcDenofProjects.ts
+++ src/viewConnectors/vcDenofProjects.ts
@@ -1,30 +1,21 @@
-import {var1,var2,res,oper} from "../reducerActions/denOfProjectsReducerActoins";
+import {setNumbers,sum,reset} from "../reducerActions/denOfProjectsReducerActoins";
 
-
-
-export const mapStateToProps = (state:any) => {
-  return {
-
-    num1:state.num1,
-    num2:state.num2,
-    result:state.result,
-    op:state.op,
-    dopState: state.denOfProjectsReducerState
-
-  }
-}
-
-
-
-
-
-export function mapDispatchToProps  (dispatch:any)  {
-  return {
-
-    var1: (val1:any) => { dispatch(var1(val1)) },
-    var2: (val2:any) => { dispatch(var2(val2)) },
-    oper: (value:any) => { dispatch(oper(value)) },
-    res: () => { dispatch(res()) },
-
-  }
+export const mapStateToProps = (state: any) => {
+  return { dopState: state.denOfProjectsReducerState };
 };
+
+export function mapDispatchToProps(dispatch: any) {
+  return {
+
+    onInputChange: (e: any) => {
+      dispatch(setNumbers(e.target));
+    },
+
+    onSum: (e:any)=>{
+      dispatch(sum(e.target));
+    },
+    onReset: ()=>{
+      dispatch(reset());
+    }
+  };
+}
